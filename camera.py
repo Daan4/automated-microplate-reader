77412883b6e194679a03f5eb0e483b34a5b5694f
Daan4 <daan_lubbers@hotmail.com>
2018-05-27 20:36:35 +0200

    photocamera->camera, update take_photo prototype to return image_path when done

---
+++ camera.py
@@ -0,0 +1,9 @@
+class Camera:
+    # todo implement raspberry pi camera?
+    def __init__(self):
+        pass
+
+    def take_photo(self):
+        """Take a photo and return the stored image path when ready"""
+        image_path = ''
+        return image_path
--- photocamera.py
+++
@@ -1,6 +0,0 @@
-class PhotoCamera:
-    def __init__(self):
-        pass
-
-    def take_photo(self):
-        pass
