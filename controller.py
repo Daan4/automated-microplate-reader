059a8139f8a58fd2f5ea908d5b6fe8ce42806c69
Daan4 <daan_lubbers@hotmail.com>
2018-05-27 21:29:53 +0200

    Edit controller to work with current caliper implementation using a blocking queue to pass the reading to the controller

--- controller.py
+++ controller.py
@@ -5,20 +5,27 @@ import time
 
 class Controller:
     """This class controls a single steppermotor-caliper feedback loop."""
-    def __init__(self, proportional_gain, integral_gain, differential_gain, sample_frequency, stepper_motor, caliper, error_margin=0.01):
+    def __init__(self, proportional_gain, integral_gain, differential_gain, stepper_motor, caliper, error_margin=0.01):
         self.pid = PID(p=proportional_gain, i=integral_gain, d=differential_gain)  # P I D controller
-        self.stepper_motor = stepper_motor  # The stepper motor moving the load
+        self.steppermotor = stepper_motor  # The stepper motor moving the load
         self.caliper = caliper  # The caliper providing position feedback.
         self.stop_loop_event = threading.Event()
-        self.sample_frequency = sample_frequency
         self.setpoint = None
         self.error_margin = error_margin  # Allowed margin of error between setpoint and measured position.
 
     def _control_loop(self):
         """The control loop, self.start and self.stop start and stop this control loop in it's own thread."""
         while not self.stop_loop_event.is_set():
-            # Get the current position
-            position = self.caliper.get_reading()
+            # Wait for the next sensor reading
+            try:
+                position = self.caliper.get_reading()
+            except TimeoutError:
+                # Timed out waiting for sensor reading
+                # Check if the process was while waiting for sensor reading
+                if self.stop_loop_event.is_set():
+                    break
+                else:
+                    raise TimeoutError("Controller timed out waiting for sensor reading")
             if self.setpoint - position < self.error_margin:
                 # If we reached the goal position -> stop the control loop
                 self.setpoint = None
@@ -35,13 +42,15 @@ class Controller:
     def start(self, setpoint):
         """Start the control loop."""
         self.stop_loop_event.clear()
+        self.caliper.start_listening()
         self.setpoint = setpoint
         threading.Thread(target=self._control_loop()).start()
 
     def stop(self):
         """Stop the control loop."""
         self.stop_loop_event.set()
-        self.stepper_motor.stop_step()
+        self.steppermotor.stop_step()
+        self.caliper.stop_listening()
 
     def wait_until_finished(self):
         self.stop_loop_event.wait()
