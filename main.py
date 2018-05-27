2c2ee8fcebc6e03cd26e4030681dba392e150098
Daan4 <daan_lubbers@hotmail.com>
2018-05-27 20:37:34 +0200

    Read setpoints from csv files, initialise camera

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from caliper import Caliper
 from controller import Controller
 from steppermotor import StepperMotor
+from camera import Camera
 import logging
 import threading
 import time
+import csv
+from tkinter import filedialog
 
+# Global references to the controllers and steppermotors for motion control
 controller_x = None
 controller_y = None
 steppermotor_z = None
 
+# Global reference to photo camera
+camera = None
+
+# Global reference to tkinter app frame
+app = None
+
+# Global reference to tkinter root frame
 tk_root = None
 
 # Set to stop process while it's running.
 stop_process_event = threading.Event()
-# Set to pause process while it's running, clear to continue the process.
+# Set to pause process while it's running after it finishes the current photo, clear to continue the process.
 pause_process_event = threading.Event()
 
 
@@ -28,20 +39,23 @@ def initialise_logging():
 
 
 def initialise_io():
-    global controller_x, controller_y, steppermotor_z
+    global controller_x, controller_y, steppermotor_z, camera
 
-    # create x-axis controller
-    caliper_x = Caliper()
+    # create x-axis controller object
+    caliper_x = Caliper(1, 1) # todo set gpio
     steppermotor_x = StepperMotor(17, 27, 22, 10)  # 10 Hz, gpio17 pulse, gpio27 direction, gpio22 interrupt
     controller_x = Controller(1, 1, 1, 1, steppermotor_x, caliper_x)
 
-    # create y-axis controller
-    caliper_y = Caliper()
+    # create y-axis controller object
+    caliper_y = Caliper(1, 1) # todo set gpio
     steppermotor_y = StepperMotor(2, 3, 4, 100)  # 100 Hz, gpio2 pulse, gpio3 direction, gpio4 interrupt
     controller_y = Controller(1, 1, 1, 1, steppermotor_y, caliper_y)
 
-    # create z-axis steppermotor
+    # create z-axis steppermotor object
     steppermotor_z = StepperMotor(0, 5, 6, 1)  # 1 Hz, gpio0 pulse, gpio5 direction, gpio6 interrupt
+
+    # create camera object
+    camera = Camera()
 
     # Calibrate steppermotors
     steppermotor_x.calibrate()
@@ -55,7 +69,7 @@ def initialise_io():
 
 def initialise_gui():
     import gui  # Avoiding circular imports
-    global tk_root
+    global tk_root, app
     tk_root = gui.tk.Tk()
     # tk_root.iconbitmap(filepath) # optional add icon
     tk_root.title('Automated Microplate Reader')
@@ -67,12 +81,20 @@ def initialise_gui():
     app.mainloop()
 
 
-def start_process(setpoints=None):
+def start_process():
     """
+    Reads setpoints from a csv file with 2 columns (x setpoint, y setpoint per well).
+    Then the camera is positioned above each well by using the two controllers.
 
     Args:
         setpoints: x and y setpoints per well in the format: [(x_setpoint, y_setpoint), ...]
     """
+    # Ask for setpoints file path
+    setpoints_filepath = filedialog.askopenfilename(filetypes=[('Setpoints csv', '*.csv')])
+    with open(setpoints_filepath) as f:
+        reader = csv.reader(f)
+        setpoints = [row for row in reader]
+
     for well in setpoints:
         setpoint_x, setpoint_y = well
         # Start control loops with given setpoints
@@ -82,7 +104,9 @@ def start_process(setpoints=None):
         controller_x.wait_until_finished()
         controller_y.wait_until_finished()
         # Take a picture and wait a bit before moving on to the next well
-        # todo implement taking pictures
+        photo_path = camera.take_photo()
+        # Show the image on screen
+        app.update_image(photo_path)
         while pause_process_event.is_set():
             # Wait for it to clear before continuing
             time.sleep(0.5)
@@ -107,5 +131,5 @@ def pause_process():
 
 if __name__ == '__main__':
     initialise_logging()
-    initialise_io()
+    #initialise_io()
     initialise_gui()
