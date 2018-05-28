52327bcf76c9d81d77a01528c346f77b9d64c8c8
Daan4 <daan_lubbers@hotmail.com>
2018-05-28 20:29:06 +0200

    implement simple camera capture to timestamped jpeg

--- camera.py
+++ camera.py
@@ -1,9 +1,21 @@
+from picamera import PiCamera
+import time
+import os
+
 class Camera:
-    # todo implement raspberry pi camera?
-    def __init__(self):
-        pass
+    def __init__(self, sleep_time=5):
+        """
+
+        Args:
+            sleep_time: Time to wait after taking a picture before continuing in seconds
+        """
+        self.camera = PiCamera()
+        self.sleep_time = sleep_time
 
     def take_photo(self):
         """Take a photo and return the stored image path when ready"""
-        image_path = ''
+        # todo better image file names, maybe structured in folder per well plate and named per well
+        image_path = os.path.join(os.path.dirname(__file__), 'pics/well_plate_{}.jpg'.format(time.time()))
+        self.camera.capture(image_path)
+        time.sleep(self.sleep_time)
         return image_path
