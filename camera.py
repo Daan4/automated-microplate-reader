65f82db50d685c4726e3998cc95b93cce81039b5
Daan <daan_lubbers@hotmail.com>
2018-05-31 11:06:52 +0200

    allow running without a camera connected

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 from picamera import PiCamera
+from picamera.exc import PiCameraError
 import time
 import os
 
@@ -9,13 +10,19 @@ class Camera:
         Args:
             sleep_time: Time to wait after taking a picture before continuing in seconds
         """
-        self.camera = PiCamera()
+        try:
+            self.camera = PiCamera()
+        except PiCameraError:
+            # Camera not connected
+            self.camera = None
         self.sleep_time = sleep_time
 
     def take_photo(self):
         """Take a photo and return the stored image path when ready"""
         # todo better image file names, maybe structured in folder per well plate and named per well
         image_path = os.path.join(os.path.dirname(__file__), 'pics/well_plate_{}.jpg'.format(time.time()))
-        self.camera.capture(image_path)
+        if self.camera is not None:
+            # todo return default test image
+            self.camera.capture(image_path)
         time.sleep(self.sleep_time)
         return image_path
