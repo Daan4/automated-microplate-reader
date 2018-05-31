9d772c1ad1db0da3f5fe742016bb0ab82eba6519
Daan <daan_lubbers@hotmail.com>
2018-05-31 11:09:45 +0200

    fix interrupt and add a delay between the clock interrupt and taking the sample from the data pin

--- caliper.py
+++ caliper.py
@@ -5,7 +5,7 @@ from queue import Queue
 
 class Caliper:
     # todo check pull up  / down and debounce settings / queue size
-    def __init__(self, pin_data, pin_clock, pin_zero, clock_bouncetime=1, pause_time=50):
+    def __init__(self, pin_data, pin_clock, pin_zero, clock_bouncetime=1, pause_time=50, delay=150):
         """Read a bit on the data pin every time a clock pulse is received.
         Return the 24-bit number that gets sent roughly every 100-150ms by the digital caliper.
 
@@ -15,6 +15,7 @@ class Caliper:
             pin_zero: zero digital output gpio pin
             clock_bouncetime: clock debounce time in ms
             pause_time: minimum time between packets in ms
+            delay: the delay between clock falling edge and taking the data sample in seconds
         """
         self.pin_data = pin_data
         self.pin_clock = pin_clock
@@ -22,19 +23,20 @@ class Caliper:
         self.clock_bouncetime = clock_bouncetime
         self.pause_time = pause_time
         self.current_burst_data = list()
-        self.last_clock_time = None
+        self.last_clock_time = 0
         self.reading_queue = Queue(1)  # Ideally the reading should always processed before the next one is ready.
+        self.delay = delay
 
         # Setup gpio
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.pin_data, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.pin_clock, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.pin_data, GPIO.IN)
+        GPIO.setup(self.pin_clock, GPIO.IN)
         GPIO.setup(self.pin_zero, GPIO.OUT)
 
     def start_listening(self):
         # Enable clock interrupt
-        GPIO.add_event_detect(self.pin_clock, GPIO.FALLING, callback=self.clock_callback, bouncetime=self.clock_bouncetime)
+        GPIO.add_event_detect(self.pin_clock, GPIO.FALLING, callback=self.clock_callback)
 
     def stop_listening(self):
         # Disable clock interrupt
@@ -62,21 +64,24 @@ class Caliper:
         time.sleep(0.1)
         GPIO.output(self.pin_zero, GPIO.LOW)
 
-    def clock_callback(self):
+    def clock_callback(self, channel):
         """
         Called on the clock falling edge.
         Sample the data input every call.
         Data is sent in 24-bit bursts every ~100-150ms.
         If listening started mid-burst the data from that first burst will be discarded.
         """
+        time.sleep(self.delay)
+        value = GPIO.input(self.pin_data)
         # If last clock was too long ago discard the current_burst_data buffer and assume a new data packet started.
         current_time = time.time()*1000.0
         if current_time - self.last_clock_time >= self.pause_time:
             self.current_burst_data = list()
         self.last_clock_time = current_time
         # Add current data bit to buffer
-        self.current_burst_data.append(GPIO.input(self.pin_data))
+        self.current_burst_data.append(value)
         # Store buffer in queue when 24 bits have been read.
+        #print(self.current_burst_data)
         if len(self.current_burst_data) == 24:
             # todo convert list of bits to distance in mm (w 2 decimals)
             self.reading_queue.put(self.current_burst_data)
