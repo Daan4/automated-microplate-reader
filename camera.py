1cd3ad9d5197d4631e541e2a7995d218f76f0b9d
Daan4 <daan_lubbers@hotmail.com>
2018-06-06 20:06:08 +0200

    remove unnecessary stuff

--- camera.py
+++ camera.py
@@ -11,19 +11,10 @@ class Camera:
             exposure_time: Time to wait after taking a picture before continuing in seconds
         """
         try:
-            # todo get camera settings from jeroen
             self.camera = PiCamera()
-            time.sleep(2)
-            # self.camera.shutter_speed = self.camera.exposure_speed
-            # self.camera.exposure_mode = "off"
-            # g = self.camera.awb_gains
-            # self.camera.awb_mode = "off"
-            # self.camera.awb_gains = g
         except PiCameraError as e:
             # Camera not connected
             self.camera = None
-            # todo remove reraise
-            raise e
         self.exposure_time = exposure_time
 
     def take_photo(self):
@@ -33,8 +24,8 @@ class Camera:
             os.mkdir('pics')
         image_path = os.path.join(os.path.dirname(__file__), 'pics/well_plate_{}.jpg'.format(time.time()))
         if self.camera is not None:
-            self.camera.start_preview()
-            time.sleep(2)
+            # self.camera.start_preview()
+            # time.sleep(2)
             # todo return default test image
             self.camera.capture(image_path)
         return image_path
