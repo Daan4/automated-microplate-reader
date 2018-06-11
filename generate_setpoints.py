cbbb34ebf948e20c4770811fc9de1fe4c20867a2
Daan4 <daan_lubbers@hotmail.com>
2018-06-11 19:08:45 +0200

    generate setpoints for 96 well, includes correction for hysteresis error on reverse direction

--- generate_setpoints.py
+++ generate_setpoints.py
@@ -2,7 +2,7 @@
 # baased on draawing 677180.pdf
 
 
-def generate_setpoints(initial_offset_y, initial_offset_x, offset_x, offset_y, rows, columns):
+def generate_setpoints(initial_offset_y, initial_offset_x, offset_x, offset_y, rows, columns, hysteresis_offset):
     """
 
     Args:
@@ -19,7 +19,13 @@ def generate_setpoints(initial_offset_y, initial_offset_x, offset_x, offset_y, r
         for y in range(rows):
             lines_buffer = []
             for x in range(columns):
-                setpoint_x = round(initial_offset_x + x * offset_x, 2)
+                # Compensate hysteresis on x axis by changing the setpoints
+                if y % 2 != 0 and x != columns-1:
+                    setpoint_x = round(initial_offset_x + x * offset_x + hysteresis_offset, 2)
+                elif y % 2 == 0 and (x == 0 and y != 0):
+                    setpoint_x = round(initial_offset_x + x * offset_x + hysteresis_offset, 2)
+                else:
+                    setpoint_x = round(initial_offset_x + x * offset_x, 2)
                 setpoint_y = round(initial_offset_y + y * offset_y, 2)
                 lines_buffer.append("{}, {}\n".format(setpoint_x, setpoint_y))
             if y % 2 != 0:
@@ -29,4 +35,5 @@ def generate_setpoints(initial_offset_y, initial_offset_x, offset_x, offset_y, r
 
 
 if __name__ == '__main__':
-    generate_setpoints(0, 0, 99/12, 99/12, 8, 12)
+    generate_setpoints(0, 0, 108/12, 108/12, 8, 12, 2.5)
+    #generate_setpoints(0, 0, 99/12, 99/12, 8, 12)
